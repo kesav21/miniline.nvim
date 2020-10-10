a30acbabf292aa2a56c7c510644c1310ea8fba29
Kesav Kadalazhi <kkadalazhi2@gmail.com>
2020-10-10 01:53:06 -0700

    initial commit

---
+++ lua/statusline.lua
@@ -0,0 +1,69 @@
+
+local M = {}
+
+local config = {
+	["n"]  = {name = " N ", hi = "%#StatusModeCyan#"},
+
+	["v"]  = {name = " V ", hi = "%#StatusModeYellow#"},
+	["V"]  = {name = " V ", hi = "%#StatusModeYellow#"},
+	[""] = {name = " V ", hi = "%#StatusModeYellow#"},
+
+	["i"]  = {name = " I ", hi = "%#StatusModeBlue#"},
+	["ic"] = {name = " I ", hi = "%#StatusModeBlue#"},
+
+	["c"]  = {name = " C ", hi = "%#StatusModePink#"},
+}
+
+setmetatable(config, {
+	__index = function(t, mode)
+		return {name = " " .. mode .. " ", hi = "%#StatusModeRed#"}
+	end
+})
+
+vim.api.nvim_command("hi StatusModeCyan   guifg=#1d2021 guibg=#689d6a gui=bold")
+vim.api.nvim_command("hi StatusModeBlue   guifg=#1d2021 guibg=#458588 gui=bold")
+vim.api.nvim_command("hi StatusModeYellow guifg=#1d2021 guibg=#d79921 gui=bold")
+vim.api.nvim_command("hi StatusModePink   guifg=#1d2021 guibg=#b16286 gui=bold")
+vim.api.nvim_command("hi StatusModeRed    guifg=#1d2021 guibg=#cc241d gui=bold")
+vim.api.nvim_command("hi StatusLight      guifg=#1d2021 guibg=#a89984")
+vim.api.nvim_command("hi StatusDark       guifg=#ebdbb2 guibg=#3c3836")
+vim.api.nvim_command("hi StatusDarker     guifg=#a89984 guibg=#282828")
+
+function readline(filename)
+	file = io.open(filename)
+	if file then
+		for line in io.lines(filename) do
+			io.close(file)
+			return line
+		end
+	end
+end
+
+function getbranch()
+	local line = readline(".git/HEAD")
+	if line then
+		return " שׂ " .. string.sub(line, 17) .. " "
+	else
+		return ""
+	end
+end
+
+function M.getline()
+	local mode = vim.api.nvim_get_mode()["mode"]
+	return config[mode]["hi"]
+		.. config[mode]["name"]
+		.. "%#StatusDark#"
+		.. getbranch()
+		.. "%#StatusDarker#"
+		.. " %F"
+		.. " %="
+		.. " %m"
+		.. " %r"
+		.. " %#StatusDark#"
+		.. " %y"
+		.. " %#StatusLight#"
+		.. " %cC"
+		.. " %l/%LL "
+end
+
+return M
---
+++ lua/tabline.lua
@@ -0,0 +1,32 @@
+
+local M = {}
+
+vim.api.nvim_command("hi TabSel    guifg=#1d2021 guibg=#a89984")
+vim.api.nvim_command("hi TabUnsel  guifg=#a89984 guibg=#282828")
+vim.api.nvim_command("hi TabClear                guibg=#282828")
+
+function gethighlight(tab, current)
+	if tab == current then return "%#TabSel#"
+	else return "%#TabUnsel#"
+	end
+end
+
+function getname(tab)
+	local win = vim.api.nvim_tabpage_get_win(tab)
+	local buf = vim.api.nvim_win_get_buf(win)
+	local fullname = vim.api.nvim_buf_get_name(buf)
+	return vim.api.nvim_call_function("fnamemodify", {fullname, ":t"})
+end
+
+function M.getline()
+	local tabs = vim.api.nvim_list_tabpages()
+	local current = vim.api.nvim_get_current_tabpage()
+
+	local line = ""
+	for _, tab in ipairs(tabs) do
+		line = line .. gethighlight(tab, current) .. " " .. getname(tab) .. " "
+	end
+	return line .. "%#TabClear#"
+end
+
+return M
